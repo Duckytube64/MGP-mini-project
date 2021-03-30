c43f437367b4d0d851df7c9ff2bd002b3b4ba967
Duckytube64 <jhijzelendoorn@gmail.com>
2021-03-30 14:36:15 +0200

    Start on droplet simulation

---
+++ MGP erosion sim/Assets/Scripts/MountainGen.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniProject
+{
+    public class MountainGen : MonoBehaviour
+    {
+        public Material mountain_Material;
+        public Texture2D hMap;
+        float[] heights;
+        SimErosion simErosion;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            List<Vector3> verts = new List<Vector3>();
+            List<int> tris = new List<int>();
+            int imgRes = hMap.width;
+            heights = new float[imgRes * imgRes];
+
+            //Bottom left section of the map, other sections are similar
+            for (int i = 0; i < imgRes; i++)
+            {
+                for (int j = 0; j < imgRes; j++)
+                {
+                    //Add each new vertex in the plane
+                    float height = hMap.GetPixel(i, j).grayscale * 40;
+                    verts.Add(new Vector3(i, height, j));
+                    heights[i * imgRes + j] = height;
+                    //Skip if a new square on the plane hasn't been formed
+                    if (i == 0 || j == 0) continue;
+                    //Adds the index of the three vertices in order to make up each of the two tris
+                    tris.Add(imgRes * i + j); //Top right
+                    tris.Add(imgRes * i + j - 1); //Bottom right
+                    tris.Add(imgRes * (i - 1) + j - 1); //Bottom left - First triangle
+                    tris.Add(imgRes * (i - 1) + j - 1); //Bottom left 
+                    tris.Add(imgRes * (i - 1) + j); //Top left
+                    tris.Add(imgRes * i + j); //Top right - Second triangle
+                }
+            }
+
+            Vector2[] uvs = new Vector2[verts.Count];
+            for (var i = 0; i < uvs.Length; i++) //Give UV coords X,Z world coords
+                uvs[i] = new Vector2(verts[i].x, verts[i].z);
+
+            GameObject plane = new GameObject("ProcPlane"); //Create GO and add necessary components
+            plane.AddComponent<MeshFilter>();
+            plane.AddComponent<MeshRenderer>();
+            Mesh procMesh = new Mesh();
+            procMesh.vertices = verts.ToArray(); //Assign verts, uvs, and tris to the mesh
+            procMesh.uv = uvs;
+            procMesh.triangles = tris.ToArray();
+            procMesh.RecalculateNormals(); //Determines which way the triangles are facing
+            plane.GetComponent<MeshFilter>().mesh = procMesh; //Assign Mesh object to MeshFilter
+            plane.GetComponent<MeshRenderer>().material = mountain_Material;
+
+            simErosion = new SimErosion(heights);
+        }
+
+        public float[] GetHeights()
+        {
+            print("bbbbbbbbbb");
+            return heights;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ MGP erosion sim/Assets/Scripts/SimErosion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniProject
+{
+    public static class Vars
+    {
+        public static float pCap, pDis, pInert = 0.5f;
+        public static float[] heights;
+        public static int imgRes;
+    }
+
+    public class SimErosion
+    {
+        public int nrDroplets = 100000;
+        public int nrIterations = 10;
+
+        // Start is called before the first frame update
+        void Start(float[] Heights)
+        {
+            Vars.heights = Heights;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (int i = 0; i < nrDroplets; i++)
+            {
+                Droplet d = new Droplet();
+                for (int j = 0; j < nrIterations; j++)
+                {
+                    int xGrid = (int)Math.Floor(d.x), yGrid = (int)Math.Floor(d.y);
+                    float xdiff = d.x - xGrid, ydiff = d.y - yGrid;
+                    float xy = Vars.heights[xGrid * Vars.imgRes + yGrid],
+                        x1y = Vars.heights[(xGrid + 1) * Vars.imgRes + yGrid],
+                        xy1 = Vars.heights[xGrid * Vars.imgRes + yGrid + 1],
+                        x1y1 = Vars.heights[(xGrid + 1) * Vars.imgRes + yGrid + 1];
+                    float dHeight = (x1y1 * xdiff + xy1 * (1 - xdiff)) * ydiff + (xy1 * xdiff + xy * (1 - xdiff)) * (1 - ydiff); // Bilinear interpolation for droplet height
+                    Vector2 gxy = new Vector2(
+                        (x1y - xy) * (1 - d.v) + (x1y1 - xy1) * d.v,
+                        (xy1 - xy) * (1 - d.u) + (x1y1 - x1y) * d.u);
+                }
+            }
+        }
+    }
+
+    public class Droplet
+    {
+        public float x, y, u = 0, v = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            System.Random r = new System.Random();
+            x = (float)r.NextDouble() * Vars.imgRes;
+            y = (float)r.NextDouble() * Vars.imgRes;
+        }
+    }
+}
