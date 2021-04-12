76335cf991eeadd3fdbccb611a6d07cf118e878b
Duckytube64 <jhijzelendoorn@gmail.com>
2021-04-12 21:03:43 +0200

    fix ui bug

--- MGP erosion sim/Assets/Scripts/SettingsMenu.cs
+++ MGP erosion sim/Assets/Scripts/SettingsMenu.cs
@@ -9,7 +9,7 @@ namespace MiniProject
 {
     public class SettingsMenu : MonoBehaviour
     {
-        string[] pars = new string[11] {"Inertia", "Gravity", "Evaporation", "Carry capacity", "Minimal slope", 
+        string[] pars = new string[11] {"Inertia", "Gravity", "Evaporation", "Carry capacity", "Minimal slope",
             "Deposition speed", "Erosion", "Erosion radius", "Update rate", "Total droplets", "Droplet lifetime"};
 
         InputField[] inputfields = new InputField[11];
@@ -75,79 +75,156 @@ namespace MiniProject
         // World settings
         public void setInertia(string inertia)
         {
-            float temp = Mathf.Clamp(float.Parse(inertia), 0, 1);
-            Vars.pInertia = temp;
-            inputfields[0].text = temp.ToString();
+            if (inertia != "")
+            {
+                float temp = Mathf.Clamp(float.Parse(inertia), 0, 1);
+                Vars.pInertia = temp;
+                inputfields[0].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[0].text = Vars.pInertia.ToString();
+            }
         }
 
         public void setGravity(string gravity)
         {
-            float temp = Mathf.Max(float.Parse(gravity), 0);
-            Vars.pGravity = temp;
-            inputfields[1].text = temp.ToString();
+            if (gravity != "")
+            {
+                float temp = Mathf.Max(float.Parse(gravity), 0);
+                Vars.pGravity = temp;
+                inputfields[1].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[1].text = Vars.pGravity.ToString();
+            }
         }
 
         public void setEvaporation(string evaporation)
         {
-            float temp = Mathf.Clamp(float.Parse(evaporation), 0, 1);
-            Vars.pEvaporation = temp;
-            inputfields[2].text = temp.ToString();
+            if (evaporation != "")
+            {
+                float temp = Mathf.Clamp(float.Parse(evaporation), 0, 1);
+                Vars.pEvaporation = temp;
+                inputfields[2].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[2].text = Vars.pEvaporation.ToString();
+            }
         }
 
         // Erosion settings
         public void setCarry(string carry)
         {
-            float temp = Mathf.Max(float.Parse(carry), 0);
-            Vars.pCapacity = temp;
-            inputfields[3].text = temp.ToString();
+            if (carry != "")
+            {
+                float temp = Mathf.Max(float.Parse(carry), 0);
+                Vars.pCapacity = temp;
+                inputfields[3].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[3].text = Vars.pCapacity.ToString();
+            }
+        }
+        public void setMinSlope(string minSlope)
+        {
+            if (minSlope != "")
+            {
+                Vars.pMinSlope = float.Parse(minSlope);
+                inputfields[4].text = minSlope;
+            }
+            else
+            {
+                inputfields[4].text = Vars.pMinSlope.ToString();
+            }
         }
 
         public void setDeposition(string deposition)
         {
-            float temp = Mathf.Clamp(float.Parse(deposition), 0, 1);
-            Vars.pDeposition = temp;
-            inputfields[4].text = temp.ToString();
+            if (deposition != "")
+            {
+                float temp = Mathf.Clamp(float.Parse(deposition), 0, 1);
+                Vars.pDeposition = temp;
+                inputfields[5].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[5].text = Vars.pDeposition.ToString();
+            }
         }
 
         public void setErosion(string erosion)
         {
-            float temp = Mathf.Clamp(float.Parse(erosion), 0, 1);
-            Vars.pErosion = temp;
-            inputfields[5].text = temp.ToString();
+            if (erosion != "")
+            {
+                float temp = Mathf.Clamp(float.Parse(erosion), 0, 1);
+                Vars.pErosion = temp;
+                inputfields[6].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[6].text = Vars.pErosion.ToString();
+            }
         }
 
         public void setRadius(string radius)
         {
-            int temp = Mathf.Max(int.Parse(radius), 0);
-            Vars.pErosionRadius = temp;
-            inputfields[6].text = temp.ToString();
-        }
-
-        public void setMinSlope(string minSlope)
-        {
-            Vars.pMinSlope = float.Parse(minSlope);
+            if (radius != "")
+            {
+                int temp = Mathf.Max(int.Parse(radius), 0);
+                Vars.pErosionRadius = temp;
+                inputfields[7].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[7].text = Vars.pErosionRadius.ToString();
+            }
         }
 
         // Simulation duration settings
         public void setDropletRate(string rate)
         {
-            int temp = Mathf.Max(int.Parse(rate), 0);
-            Vars.dropletsPerUpdate = temp;
-            inputfields[8].text = temp.ToString();
+            if (rate != "")
+            {
+                int temp = Mathf.Max(int.Parse(rate), 0);
+                Vars.dropletsPerUpdate = temp;
+                inputfields[8].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[8].text = Vars.dropletsPerUpdate.ToString();
+            }
         }
 
         public void setTotalDroplets(string total)
         {
-            int temp = Mathf.Max(int.Parse(total), 0);
-            Vars.totalDroplets = temp;
-            inputfields[9].text = temp.ToString();
+            if (total != "")
+            {
+                int temp = Mathf.Max(int.Parse(total), 0);
+                Vars.totalDroplets = temp;
+                inputfields[9].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[9].text = Vars.totalDroplets.ToString();
+            }
         }
 
         public void setLifeTime(string time)
         {
-            int temp = Mathf.Max(int.Parse(time), 0);
-            Vars.nrIterations = temp;
-            inputfields[10].text = temp.ToString();
+            if (time != "")
+            {
+                int temp = Mathf.Max(int.Parse(time), 0);
+                Vars.nrIterations = temp;
+                inputfields[10].text = temp.ToString();
+            }
+            else
+            {
+                inputfields[10].text = Vars.nrIterations.ToString();
+            }
         }
     }
 }
